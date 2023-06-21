be382b35cc24c0d720d2eaa725e4891fa335f01b
Newton Galiza <newtonjgaliza@gmail.com>
2023-06-21 15:56:27 -0300

    part 3
    part 3

--- 2DPlatformer/Assets/Scripts/PlayerController.cs
+++ 2DPlatformer/Assets/Scripts/PlayerController.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator anim;
 
     private float movementInputDirection;
-    public float movementSpeed = 10;
     private bool isFacingRight = true;
-    public float jumpForce = 16;
+    private bool isWalking;
+    private bool isGrounded;
+    private bool canJump;
+
+    [SerializeField] private int amountOfJumps = 1;
+    private int amountOfJumpsLeft;
+
+    public float movementSpeed = 10f;    
+    public float jumpForce = 16f;
+    public float groundCheckRadius;
+
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();        
+        rb = GetComponent<Rigidbody2D>();  
+        anim = GetComponent<Animator>();
+        amountOfJumpsLeft = amountOfJumps;
     }
 
     // Update is called once per frame
@@ -22,11 +37,21 @@ public class PlayerController : MonoBehaviour
     {
         CheckInput();
         CheckMovementDirection();
+        UpdateAnimations();
+        CheckIfCanJump();
     }
 
     private void FixedUpdate() 
     {
         ApplyMovement();        
+        CheckSurroundings();
+    }
+
+    private void UpdateAnimations()
+    {
+        anim.SetBool("isWalking", isWalking);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetFloat("yVelocity", rb.velocity.y);
     }
 
     private void CheckInput()
@@ -41,7 +66,11 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if(canJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            amountOfJumpsLeft--;
+        }
     }
 
     private void ApplyMovement()
@@ -59,6 +88,15 @@ public class PlayerController : MonoBehaviour
         {
             Flip();
         }
+
+        if(rb.velocity.x != 0)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
     }
 
     private void Flip()
@@ -68,4 +106,32 @@ public class PlayerController : MonoBehaviour
         transform.Rotate(0.0f, 180.0f, 0.0f);
     }
 
+    private void CheckSurroundings()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    private void OnDrawGizmos() 
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+
+    private void CheckIfCanJump()
+    {
+        if(isGrounded && rb.velocity.y <= 0)
+        {
+            amountOfJumpsLeft = amountOfJumps;
+            canJump = true;            
+        }
+
+        if(amountOfJumpsLeft <= 0)
+        {
+            canJump = false;
+        }
+        else
+        {
+            canJump = true;
+        }
+        
+    }
 }
